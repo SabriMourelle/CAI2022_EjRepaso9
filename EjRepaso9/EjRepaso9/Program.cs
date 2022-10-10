7ac9ff5059a64d46b39999d10486b8741eeef197
SabriMourelle <89493758+SabriMourelle@users.noreply.github.com>
2022-10-10 20:59:45 -0300

    ej9

---
+++ EjRepaso9/EjRepaso9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjRepaso9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Ejercicio IX
+            //Crear una aplicación que pida al usuario su nombre e indique por pantalla “¡Hola,
+            //*usuario * !” si ingresa su nombre, en caso contrario dirá “No te conozco”.
+            //Por ejemplo, si el usuario es Suyai, e ingresa el nombre “Suyai” dirá “¡Hola, Suyai!”, si
+            //no, “No te conozco”
+
+            string usuario="Sabrina";
+            string valor;
+
+            Console.WriteLine("Ingrese su nombre");
+            valor = Console.ReadLine();
+            if (valor == usuario)
+                Console.WriteLine("Hola "+ usuario+ "!");
+            else
+                Console.WriteLine("No te conozco");
+            Console.ReadKey();
+        }
+    }
+}
